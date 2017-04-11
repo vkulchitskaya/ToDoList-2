92656f1e587b4c8fed9c52e48f9df2ff1db2394f
Valeriya Kulchitskaya <arealise@gmail.com>
2017-04-11 13:12:36 +0300

    add checkbox, try fix delete,edit task

--- src/js/template.js
+++ src/js/template.js
@@ -6,9 +6,10 @@ export class Template {
         return tasks.reduce((a,item) => a + `
             
             <li data-id='${item.id}' ${item.done ? 'class=checked' : ''}>
-                <span class='task'>${item.name}</span>
-                <input class='check' type='checkbox' ${item.done ? 'checked' : ''}>
-                <span class='close'>&#215</span>
+                    <label><input class='checkbox' type='checkbox' ${item.done ? 'checked' : ''}>
+                    <span class="checkbox-custom"></span>
+                    <span class='task'>${item.name}</span>
+                    <span class='close'>&#215</span></label>               
             </li>`, '');
     }
 }
